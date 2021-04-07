f933c81c594cb93e29afd5aaef753a0fd2d6b7cb
Florian Göbel <florian.goebel@outlook.de>
2021-04-07 23:59:18 +0200

    Aktualisiert: Eslint Fehler behoben.

--- src/auth/service/film.service.ts
+++ src/auth/service/film.service.ts
@@ -1,17 +1,17 @@
-/* 
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 import { QueryOptions } from 'mongoose';
 import { Film, FilmData } from '../../film/entity/film';
@@ -67,7 +67,7 @@ export class FilmService {
      */
     private async checkIdAndVersion(id: string, version: number) {
         const filmDb: FilmData | null = await FilmModel.findById(id).lean();
-        if (filmDb === null) {
+        if (filmDb === undefined) {
             const result = new FilmNotExists(id);
             logger.debug("FilmService.checkIdAndVersion(): FilmNotExists=%o", result)
             return result;
