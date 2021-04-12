f6484edbaea2000441a831b54dd79c983ef85b7f
Taner Memili <Taner.Memili@web.de>
2021-04-12 22:21:07 +0200

    Update Security; HTML Funktionen für EJS; App-Klasse

---
+++ src/film/html/suche.ts
@@ -0,0 +1,38 @@
+/*
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Das Modul besteht aus der Suchfunktion für EJS.
+ * @packageDocumentation
+ */
+
+import type { Request, Response } from 'express';
+import { FilmService } from '../service/film.service';
+import { logger } from './../../shared/logger';
+
+const filmService = new FilmService();
+
+/**
+ * Asynchrone Suchfunktion für EJS, um alle Bücher zu suchen.
+ *
+ * @param req Request-Objekt von Express mit der URL für EJS
+ * @param res Response-Objekt von Express
+ */
+export const suche = async (req: Request, res: Response) => {
+    logger.error('suche(): %s', req.url);
+    const filme = await filmService.find();
+    res.render('suche', { title: 'Suche', filme });
+};
