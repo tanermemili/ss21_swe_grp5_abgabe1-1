c92160ae074936c6ba303cb49aa2e44afb06172f
Lisella <maus.lisa@gmx.de>
2021-04-13 20:28:52 +0200

    index.ts

--- src/film/rest/index.ts
+++ src/film/rest/index.ts
@@ -15,7 +15,7 @@ const fileHandler = new FilmFileRequestHandler();
 /**
  * Asynchrone Filmsuche anhand der ID als Pfadparameter
  * Rückgabe 304, 200 oder 404
- * 
+ *
  * @param req Request-Objekt von Express
  * @param res Leeres Response-Objekt von Express
  * @returns Leeres Promise-Objekt
@@ -23,3 +23,65 @@ const fileHandler = new FilmFileRequestHandler();
 
 export const findById = (req: Request, res: Response) =>
     handler.findById(req, res);
+
+
+
+/**
+ *
+ * @param req Request-Objekt von Express.
+ * @param res Leeres Response-Objekt von Express.
+ * @returns Leeres Promise-Objekt.
+ */
+export const find = (req: Request, res: Response) => handler.find(req, res);
+
+/**
+ *
+ * Falls Constraints verletzt sind, wird der Statuscode `400` (`Bad Request`)
+ * gesetzt und genauso auch wenn der Titel oder die ISBN-Nummer bereits
+ * existieren.
+ *
+ * @param req Request-Objekt von Express.
+ * @param res Leeres Response-Objekt von Express.
+ * @returns Leeres Promise-Objekt.
+ */
+export const create = (req: Request, res: Response) => handler.create(req, res);
+
+/**
+
+ *
+ * @param req Request-Objekt von Express.
+ * @param res Leeres Response-Objekt von Express.
+ * @returns Leeres Promise-Objekt.
+ */
+export const update = (req: Request, res: Response) => handler.update(req, res);
+
+/**
+ * Ein Buch wird anhand seiner ID-gelöscht, die als Pfad-Parameter angegeben
+ * ist. Der zurückgelieferte Statuscode ist `204` (`No Content`).
+ *
+ * @param req Request-Objekt von Express.
+ * @param res Leeres Response-Objekt von Express.
+ * @returns Leeres Promise-Objekt.
+ */
+export const deleteFn = (req: Request, res: Response) =>
+    handler.delete(req, res);
+
+/**
+
+ * (`No Content`) gesetzt.
+ *
+ * @param req Request-Objekt von Express.
+ * @param res Leeres Response-Objekt von Express.
+ */
+export const upload = (req: Request, res: Response) =>
+    fileHandler.upload(req, res);
+
+/**
+
+ *
+ * @param req Request-Objekt von Express.
+ * @param res Leeres Response-Objekt von Express.
+ * @returns Leeres Promise-Objekt.
+ */
+export const download = (req: Request, res: Response) =>
+    fileHandler.download(req, res);
