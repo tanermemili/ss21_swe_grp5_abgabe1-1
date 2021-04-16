a9f95464c01950e55c56a9b15f680e1e3bcdae64
Lisella <maus.lisa@gmx.de>
2021-04-16 08:16:28 +0200

    tests DELETE, GET und POST

--- test/testserver.ts
+++ test/testserver.ts
@@ -16,7 +16,7 @@
  */
 
 import { Agent, createServer } from 'https';
-import { connectDB, logger, populateDB, serverConfig } from '../src/shared';
+import { connectDB, logger, populateDB, nodeConfig } from '../src/shared';
 import type { AddressInfo } from 'net';
 import type { RequestListener } from 'http';
 import type { SecureContextOptions } from 'tls';
@@ -26,14 +26,13 @@ import { app } from '../src/app';
 // -----------------------------------------------------------------------------
 // T e s t s e r v e r   m i t   H T T P S   u n d   R a n d o m   P o r t
 // -----------------------------------------------------------------------------
-const { host, dev } = serverConfig;
 let server: Server;
 
 export const createTestserver = async () => {
-    await populateDB(dev);
+    await populateDB();
     await connectDB();
 
-    const { cert, key } = serverConfig;
+    const { cert, key } = nodeConfig;
     // Shorthand Properties
     const options: SecureContextOptions = { key, cert, minVersion: 'TLSv1.3' };
     server = createServer(options, app as RequestListener)
@@ -42,6 +41,7 @@ export const createTestserver = async () => {
             logger.info(`Node ${process.version}`);
             const address = server.address() as AddressInfo;
             if (address !== null && typeof address !== 'string') {
+                const { host } = nodeConfig;
                 logger.info(
                     `Testserver ist gestartet: https://${host}:${address.port}`,
                 );
